c672ce27023a04b4f36e1b00f94254bcb947c70c
bevannutakor <bevannutakor@icloud.com>
2022-06-05 00:10:55 -0700

    finally have direction
    Simple buy and sell automation, currently trying to fix api key errors

--- buy_crypto.py
+++ buy_crypto.py
@@ -1,11 +1,24 @@
 import os
-from binance.enums import *
+from binance.client import Client
+# - Create a tkinter UI
+# - Allow user input
+# - Binance is so weirdly confusing when buying and selling crypto so I am trying to change that
+api_key = os.environ.get('API_KEY')
+api_secret = os.environ.get('SECRET_KEY')
+#testing interaction with the exchange
+client = Client(api_key, api_secret, testnet=True)
+client.API_URL = 'https://testnet.binance.vision/api'
 
-#initialization
-#api_key = os.environ.get('API_KEY')
-#api_secret = os.environ.get('SECRET_KEY')
+#validate buy but don't send to exchange
+def buy():
+    #amount = input("type the quantity you would like to buy: ")
+    #coin = int(input("type the coin symbol: "))
 
-testnet_key = os.environ.get('HMAC_KEY')
-testnet_secret = os.environ.get('HMAC_SECRET')
+    order = client.create_test_order(
+        symbol='BNBBTC',
+        side=Client.SIDE_BUY,
+        type=Client.ORDER_TYPE_MARKET,
+        quantity=100)
 
-client = Client(testnet_key, testnet_secret)
+def sell():
+    pass
